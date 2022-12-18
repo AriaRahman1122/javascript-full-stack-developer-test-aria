ecc2ec22f830c7883cda2f455e271187da688d38
Aria Rahman <aria.rahman1122@gmail.com>
2022-12-18 15:17:18 +0700

    movie card and style

--- test2/project-test2/data/movie.ts
+++ test2/project-test2/data/movie.ts
@@ -10,15 +10,15 @@ export type Movie = {
 export const movies:Movie[] = [
     {
         id: 1,
-        image : "https://resizing.flixster.com/E6CxxOqnh09qsuvND7dXpVwYOGM=/206x305/v2/https://flxt.tmsimg.com/assets/p10951814_p_v8_af.jpg",
-        title: "Ant Man",
-        rate: 9,
+        image : "https://cdn.oneesports.gg/cdn-data/2022/12/Anime_DemonSlayer_SwordsmithVillageArc.jpg",
+        title: "Demon Slayer",
+        rate: 10,
         description: "hsaubsncjcikjdcs",
     },
     {
         id: 2,
-        image: "https://lumiere-a.akamaihd.net/v1/images/h_blackpanther_mobile_19754_57fe2288.jpeg?region=0,0,640,480",
-        title: "Black Panther",
+        image: "https://pic0.iqiyipic.com/image/20211206/f2/2e/a_100421840_m_601_en_m1_1080_608.jpg",
+        title: "One Piece",
         rate: 8,
         description: "hsaubsncjcikjdcs",
     },
@@ -27,6 +27,27 @@ export const movies:Movie[] = [
         image: "https://media.suara.com/pictures/653x366/2022/12/01/37551-poster-kupu-kupu-malam-twittermdentertainment.jpg",
         title: "Kupu-Kupu Malam",
         rate: 5,
+        description: "hsaubsncjcikjdcs",
+    },
+    {
+        id: 4,
+        image: "https://media.suara.com/pictures/653x366/2022/12/01/37551-poster-kupu-kupu-malam-twittermdentertainment.jpg",
+        title: "Kupu-Kupu Malam",
+        rate: 5,
+        description: "hsaubsncjcikjdcs",
+    },
+    {
+        id: 2,
+        image: "https://pic0.iqiyipic.com/image/20211206/f2/2e/a_100421840_m_601_en_m1_1080_608.jpg",
+        title: "One Piece",
+        rate: 8,
+        description: "hsaubsncjcikjdcs",
+    },
+    {
+        id: 1,
+        image : "https://cdn.oneesports.gg/cdn-data/2022/12/Anime_DemonSlayer_SwordsmithVillageArc.jpg",
+        title: "Demon Slayer",
+        rate: 10,
         description: "hsaubsncjcikjdcs",
     }
 ]
--- test2/project-test2/pages/movies.tsx
+++ test2/project-test2/pages/movies.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Card, Row, Col } from 'antd';
+import styles from '../styles/Home.module.css'
 
 
 function MovieList() {
@@ -16,19 +18,27 @@ function MovieList() {
   }, []);
 
   return (
-    <ul>
-      {movies.map(movie => (
-          <li key={movie.id}>
-          <img src={movie.image} alt="" />
-          {movie.title}
-          {movie.rate}
-          {movie.description}
-        </li>
-
+    <div className="site-card-wrapper" id={styles.homeMovies}>
+    <Row gutter={10}>
+    {movies.map(movie => (
+        <Col span={5}>
+        <Card
+        hoverable
+        style={{ width: 240}}
+        cover={<img alt="example" src={movie.image}/>}
+        >
+          <h3>{movie.title}</h3>
+          <p>{movie.rate}</p>
+          <p>{movie.description}</p>
+      </Card>
+      <br />
+      </Col>
         
         
-      ))}
-    </ul>
+        )
+        )}
+    </Row>
+  </div>
   );
 }
 
